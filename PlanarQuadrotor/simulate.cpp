99583fc6ec82d8a94fac4d3937befed0d76c806a
Pawel Reich <thegitman@outlook.com>
2023-05-18 15:20:28 +0200

    Removed FPS counter, hardware acceleration if MSVC

--- PlanarQuadrotor/simulate.cpp
+++ PlanarQuadrotor/simulate.cpp
@@ -69,8 +69,6 @@ int main(int argc, char* args[])
     std::vector<float> y_history;
     std::vector<float> theta_history;
 
-    Uint32 last_ticks = 0;
-
     if (init(gWindow, gRenderer, SCREEN_WIDTH, SCREEN_HEIGHT) >= 0)
     {
         SDL_Event e;
@@ -81,10 +79,6 @@ int main(int argc, char* args[])
 
         while (!quit)
         {
-            Uint32 t = SDL_GetTicks();
-            std::cout << 1.f / ((float)(t - last_ticks)  / 1000.f)<< std::endl;
-            last_ticks = t;
-
             //events
             while (SDL_PollEvent(&e) != 0)
             {
@@ -121,11 +115,16 @@ int main(int argc, char* args[])
 
 int init(std::shared_ptr<SDL_Window>& gWindow, std::shared_ptr<SDL_Renderer>& gRenderer, const int SCREEN_WIDTH, const int SCREEN_HEIGHT)
 {
+    SDL_RendererFlags sdl_renderer = SDL_RENDERER_ACCELERATED;
+    #ifndef _MSC_VER
+    sdl_renderer = SDL_RENDERER_SOFTWARE;
+    #endif
+
     if (SDL_Init(SDL_INIT_VIDEO) >= 0)
     {
         SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
         gWindow = std::shared_ptr<SDL_Window>(SDL_CreateWindow("Planar Quadrotor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN), SDL_DestroyWindow);
-        gRenderer = std::shared_ptr<SDL_Renderer>(SDL_CreateRenderer(gWindow.get(), -1, SDL_RENDERER_SOFTWARE), SDL_DestroyRenderer);
+        gRenderer = std::shared_ptr<SDL_Renderer>(SDL_CreateRenderer(gWindow.get(), -1, sdl_renderer), SDL_DestroyRenderer);
         SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
     }
     else
